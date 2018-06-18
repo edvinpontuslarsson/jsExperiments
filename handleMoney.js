b4854e6cf71d7ec288f9e4c1b97cbe17d066ed7a
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-06-18 10:36:26 +0200

    Updates handleMoney

--- handleMoney.js
+++ handleMoney.js
@@ -1,38 +1,57 @@
 const basket = []
 
 const milk = {
-  product: 'milk',
+  type: 'milk',
   dollars: 0,
   cents: 99
 }
 
 const chickenWings = {
-  product: 'chicken wings',
+  type: 'chicken wings',
   dollars: 4,
   cents: 0
 }
 
 const rice = {
-  product: 'rice',
+  type: 'rice',
   dollars: 1,
   cents: 99
 }
 
-putInBasket(basket, milk)
-putInBasket(basket, milk)
-putInBasket(basket, rice)
-putInBasket(basket, chickenWings)
+;(() => {
+  putInBasket(basket, milk)
+  putInBasket(basket, milk)
+  putInBasket(basket, rice)
+  putInBasket(basket, chickenWings)
 
-const sum = getSum(basket)
+  const sum = getSum(basket)
 
-console.log(basket)
-console.log(`
-Sum: ${sum.dollars}$ and ${sum.cents} cents
-`)
+  console.log(basket)
+  console.log(`
+  Sum: ${sum.dollars}$ and ${sum.cents} cents
+  `)
+})()
 
 function putInBasket (basket, product) {
-  const i = basket.length
-  basket[i] = product
+  let exists = false
+
+  for (let i = 0; i < basket.length; i += 1) {
+    if (basket[i].type === product.type) {
+      exists = true
+
+      basket[i].dollars *= 2
+      basket[i].cents *= 2
+      basket[i].quantity += 1
+
+      centsToDollars(basket[i])
+    }
+  }
+
+  if (!exists) {
+    const j = basket.length
+    basket[j] = product
+    basket[j].quantity = 1
+  }  
 }
 
 function getSum (basket) {
@@ -46,10 +65,18 @@ function getSum (basket) {
     sum.cents += basket[i].cents
   }
 
-  while (sum.cents > 100) {
-    sum.cents -= 100
-    sum.dollars += 1
-  }
+  centsToDollars(sum)
 
   return sum
+}
+
+/**
+ * 
+ * @param {*} object 
+ */
+function centsToDollars (object) {
+  while (object.cents > 100) {
+    object.cents -= 100
+    object.dollars += 1
+  }
 }
